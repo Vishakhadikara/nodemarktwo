41e3ae97abf9e15c4919fd99d7b0ad8811052343
VIZBLIZ <44446283+Vishakhadikara@users.noreply.github.com>
2023-05-19 20:24:55 +0530

    socket

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('hello', () => {
+        console.log('Hello received from a user');
+    });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    io.on('connection', (socket) => {
+        console.log('A user connected');
+
+        socket.on('disconnect', () => {
+            console.log('A user disconnected');
+        });
+    });
+
+    http.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
 });
